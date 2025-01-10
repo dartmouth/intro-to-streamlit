a2036d1d56080aef6017fd324b348af284d8cb97
Simon Stone <simon.stone@dartmouth.edu>
2025-01-10 15:56:06 -0500

    set up structure; draft basics

--- src/01-streamlit-basics.py
+++ src/01-streamlit-basics.py
@@ -1,30 +1,80 @@
 import streamlit as st
 
 
-def say_hi(name):
-    st.write(f"Hi, {name}! 👋")
-
-
-def say_whatsup(name):
-    st.write(f"What's up, {name}? 😎")
+def say_hi(name, emoji):
+    st.write(f"Hi, {name}! {emoji}")
 
 
 st.title("Streamlit: Basic elements")
 
-user_name = st.text_input(label="What's your name?")
+st.markdown(
+    """
+    ## What is this?
 
-col1, col2 = st.columns(2)
+    This is a :streamlit: Streamlit web application that showcases some
+    basic elements of Streamlit!
+    """
+)
 
-with col1:
-    do_say_hi = st.button(label="Say hi!", use_container_width=True, type="primary")
+st.divider()
 
-with col2:
-    do_say_whatsup = st.button(
-        label="Say what's up!", use_container_width=True, type="secondary"
-    )
+st.markdown(
+    """
+    There is great support for :rainbow[GitHub-flavored] Markdown.
+
+    You can also use [LaTeX expressions](https://katex.org/docs/supported.html), which is great for formulas:
+
+    $$\\sqrt{-1} \\cdot 2^3 \\sum \\pi = \\text{delicious}$$
+
+    You can use [emojis](https://share.streamlit.io/streamlit/emoji-shortcodes), which is :100: % :sunglasses:.
+
+    You can even use [Google Material Symbols](https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded)
+    like :material/search: :material/home: :material/delete:
+    """
+)
+
+st.divider()
+
+st.markdown("## Input elements")
+
+user_name = st.text_input(label="What's your name?", placeholder="Type your name here!")
+
+emoji = st.radio(
+    label="Choose your style", options=[":wave:", ":sunglasses:", ":heart:"]
+)
+
+do_say_hi = st.button(label="Say hi!", use_container_width=True, type="primary")
+
+if not user_name and do_say_hi:
+    st.warning("Tell me your name first!")
 
 if user_name and do_say_hi:
-    say_hi(user_name)
+    say_hi(user_name, emoji)
 
-if user_name and do_say_whatsup:
-    say_whatsup(user_name)
+st.divider()
+
+confusion_level = st.slider(
+    label="Current confusion level", min_value=0, max_value=1_000_000
+)
+
+current_confusion = "Current confusion: "
+if confusion_level < 100_000:
+    current_confusion += ":sunglasses:"
+elif confusion_level < 500_000:
+    current_confusion += ":thinking_face:"
+else:
+    current_confusion += ":face_with_spiral_eyes:"
+
+current_confusion
+
+st.divider()
+
+streamlit_day = st.date_input(label="The day I learned about Streamlit")
+
+pic = st.camera_input(label="What I looked like learning about Streamlit!")
+
+
+if pic:
+    st.download_button(
+        label="Save pic", data=pic, file_name=f"{streamlit_day}-streamlit.jpg"
+    )
