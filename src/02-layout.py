f292c0f7f5987c70bfb8a712d5bdb22d6f9bbbc8
Simon Stone <simon.stone@dartmouth.edu>
2025-01-11 16:44:45 -0600

    demonstrate some basic layout features

--- src/02-layout.py
+++ src/02-layout.py
@@ -1,1 +1,18 @@
-# TODO
+import streamlit as st
+
+st.title("Customizing the layout of a Streamlit app")
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.write("Here is column 1")
+
+with col2:
+    st.button("column 2", use_container_width=True)
+
+with col3:
+    with st.container(border=True):
+        st.radio(label="letters", options=["a", "b", "c"], horizontal=True)
+
+with st.expander(label="Additional options"):
+    st.slider(label="Number", min_value=0, max_value=10)
